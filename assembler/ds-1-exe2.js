0e688dcf4a39742068cc72917800dbdfdcb42703
Marcel <googlillo@gmail.com>
2020-11-12 18:29:59 +0100

    added new exercice

--- assembler/ds-1-exe2.js
+++ assembler/ds-1-exe2.js
@@ -1,3 +1,16 @@
 // Write a function that returns an array with the first 2 keys of the following object:
 
-const product = { name: 'Sony WH-1000XM3', price: '300.00', itemsInStock: 23 };
+const product = {name: 'Sony WH-1000XM3', price: '300.00', itemsInStock: 23};
+const arrObject = Object.entries(product);
+const mod = {}
+
+function firstTwo(object) {
+    const arrObject = Object.entries(object);
+    const mod = {}
+    for (let i = 0; i < 2; i++) {
+        mod[arrObject[i][0]] = arrObject[i][1];
+    };
+    return mod;
+};
+
+console.log(firstTwo(product))
